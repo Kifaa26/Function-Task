e2584e57899b58ff38ae5f3b870d400c28695438
Kifaa26 <akifahpalekar@gmail.com>
2024-05-23 09:21:36 +0200

    functions

---
+++ script/code.js
@@ -0,0 +1,16 @@
+function greet(name) {
+    console.log ("Hello" + " " + name)
+}
+greet("Aakifah")
+
+function multiply (numb1, numb2) {
+    console.log(numb1 * numb2);
+}
+multiply(9,4);
+
+function tableOfFive(num) {
+let result = num * 5
+console.log(result);
+return result;
+}
+tableOfFive(6)
